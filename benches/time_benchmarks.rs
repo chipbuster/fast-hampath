0b0a14e0904f91eebb04232f476bc78cc5070d49
Kevin Song <chips@ksong.dev>
2021-12-20 11:42:13 -0600

    Add multiple benchmarks

--- benches/time_benchmarks.rs
+++ benches/time_benchmarks.rs
@@ -2,8 +2,28 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use fast_hampath::fastpath::HampathBuilder;
 use typed_arena::Arena;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("solve n=100", |b| b.iter(|| solve_random_hampath(black_box(100))));
+pub fn benchmark_100(c: &mut Criterion) {
+    c.bench_function("solve n=100", |b| {
+        b.iter(|| solve_random_hampath(black_box(100)))
+    });
+}
+
+pub fn benchmark_200(c: &mut Criterion) {
+    c.bench_function("solve n=200", |b| {
+        b.iter(|| solve_random_hampath(black_box(200)))
+    });
+}
+
+pub fn benchmark_50(c: &mut Criterion) {
+    c.bench_function("solve n=50", |b| {
+        b.iter(|| solve_random_hampath(black_box(50)))
+    });
+}
+
+pub fn benchmark_10(c: &mut Criterion) {
+    c.bench_function("solve n=10", |b| {
+        b.iter(|| solve_random_hampath(black_box(50)))
+    });
 }
 
 fn solve_random_hampath(n: usize) {
@@ -11,5 +31,30 @@ fn solve_random_hampath(n: usize) {
     HampathBuilder::new_random(n, &a).solution_pair();
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn sleep_short_time(c: &mut Criterion) {
+    c.bench_function("sleep 500μs", |b| {
+        b.iter(|| std::thread::sleep(black_box(std::time::Duration::from_micros(500))))
+    });
+}
+
+fn sleep_long_time(c: &mut Criterion) {
+    c.bench_function("sleep 25ms", |b| {
+        b.iter(|| std::thread::sleep(black_box(std::time::Duration::from_millis(25))))
+    });
+}
+
+criterion_group! {
+    name = sleep_benches;
+    config = Criterion::default();
+    targets = sleep_short_time, sleep_long_time
+}
+
+criterion_group!(
+    solve_benches,
+    benchmark_10,
+    benchmark_50,
+    benchmark_100,
+    benchmark_200
+);
+
+criterion_main!(solve_benches, sleep_benches);
